5c57e2590c00a723996caf1b481e92f28c0c2a2a
Linepocharpe <paulicharpe@gmail.com>
2023-10-25 16:16:16 +0200

    Incorporation of data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Codon table data
+#'
+#' Table giving the correspondance between a codon and the amino acid it represents
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
